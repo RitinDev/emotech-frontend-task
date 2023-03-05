a3668bf24d92e9f25cf3620271355f7f1c2f4e28
Ritin Malhotra <rm5486@nyu.edu>
2023-03-05 05:39:13 +0400

    Add Sidebar Active Icon State

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,26 +1,34 @@
 import './Sidebar.css';
 import viteLogo from '../images/vite.svg';
 
+import { useState } from 'react';
+
 import { HomeOutlined, AppstoreAddOutlined, ClockCircleOutlined, UserOutlined } from '@ant-design/icons';
 
 const Sidebar = () => {
+    const menuItems = [
+        { icon: <HomeOutlined />},
+        { icon: <AppstoreAddOutlined /> },
+        { icon: <ClockCircleOutlined /> },
+        { icon: <UserOutlined /> },
+    ];
+    const [activeItem, setActiveItem] = useState(0);
+
     return (
         <div className='sidebar'>
             <img src={viteLogo} alt='Vite Logo' className='vite-logo' />
             <ul className='sidebar-menu'>
-                <li className='sidebar-menu-item'>
-                    <HomeOutlined />
-                </li>
-                <li className='sidebar-menu-item'>
-                    <AppstoreAddOutlined />
-                </li>
-                <li className='sidebar-menu-item'>
-                    <ClockCircleOutlined />
-                </li>
-                <li className='sidebar-menu-item'>
-                    <UserOutlined />
-                </li>
+                {menuItems.map((item, index) => (
+                    <li
+                        key={index}
+                        className={`sidebar-menu-item ${activeItem === index ? 'active' : ''}`}
+                        onClick={() => setActiveItem(index)}
+                    >
+                        {item.icon}
+                    </li>
+                ))}
             </ul>
+
         </div>
     );
 }
