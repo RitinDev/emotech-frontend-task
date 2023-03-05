c0362b93bc3d4d9938c9d54e75559d7f8ad16083
Ritin Malhotra <rm5486@nyu.edu>
2023-03-05 20:41:03 +0400

    Add Directory Location Button

--- src/components/RightSidebar.js
+++ src/components/RightSidebar.js
@@ -1,11 +1,29 @@
-import './RightSidebar.css'
+import React, { useState } from 'react';
+import './RightSidebar.css';
+import { Input } from 'antd';
+import { FolderOutlined, RightOutlined } from '@ant-design/icons';
 
-const RightSidebar = () => {    
+const RightSidebar = () => {
+    const [directoryPath, setDirectoryPath] = useState('');
+
+    const handleDirectoryChange = (event) => {
+        setDirectoryPath(event.target.value);
+    }
     return (
         <div className="right-sidebar">
             <p className='output-settings-header'>Output Settings</p>
-            <div className="file-location">
-                <p className='file-location-header'>Location</p>
+            <div className="directory-location">
+                <p className='directory-location-header'>Location</p>
+                <div className="file-location-input">
+                    <span className="input-prefix"><FolderOutlined style={{background: '#e3ecf1'}}/></span>
+                    <Input
+                        style={{ backgroundColor: '#e3ecf1' }}
+                        placeholder="Choose Directory"
+                        value={directoryPath}
+                        onChange={handleDirectoryChange}
+                    />
+                    <span className="input-suffix"><RightOutlined style={{background: '#e3ecf1'}} /></span>
+                </div>
             </div>
         </div>
     )
