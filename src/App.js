47f987b11b25cf8507ad5d3d7dd7fef1c7f36d23
Ritin Malhotra <rm5486@nyu.edu>
2023-03-05 15:39:52 +0400

    Add Right Sidebar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 // Static imports
 import './App.css'
+import RightSidebar from './components/RightSidebar';
 
 // Import components
 import Sidebar from './components/Sidebar'
 
 function App() {
   return (
-    <div className="container">
+    <div className="app-container">
       <h1>Hello World</h1>
       <Sidebar />
+      <RightSidebar />
     </div>
   );
 }
---
+++ src/components/RightSidebar.js
@@ -0,0 +1,14 @@
+import './RightSidebar.css'
+
+const RightSidebar = () => {    
+    return (
+        <div className="right-sidebar">
+            <p className='output-settings-header'>Output Settings</p>
+            <div className="file-location">
+                <p className='file-location-header'>Location</p>
+            </div>
+        </div>
+    )
+}
+
+export default RightSidebar
