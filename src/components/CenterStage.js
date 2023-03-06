3ff919a438a423d7e8ee4d7ee3fe0f20a85a3fff
Ritin Malhotra <rm5486@nyu.edu>
2023-03-06 13:26:51 +0400

    Add Image Mic

--- src/components/CenterStage.js
+++ src/components/CenterStage.js
@@ -1,4 +1,5 @@
 import './CenterStage.css'
+import MicImage from '../images/mic.png'
 
 import { CaretLeftFilled } from '@ant-design/icons';
 import { Image } from 'antd';
@@ -11,11 +12,37 @@ const CenterStage = () => {
         <div className="center-stage">
             <p className="untitled"><CaretLeftFilled /> Untitled</p>
             <div className="center-stage-image-container">
-                <Image className='center-stage-image'
+                <Image className='center-stage-background'
                     src={testImage}
                     width='100%' height='100%'
-                    style={{ objectFit: 'cover', borderRadius: '12.5px' }}
+                    style={{
+                        objectFit: 'cover',
+                        borderRadius: '12.5px',
+                        filter: 'opacity(0.75)',
+                    }}
                     preview={false} />
+                <Image className='center-stage-mic'
+                    src={MicImage}
+                    width='20%' height='90%'
+                    style={{
+                        position: 'absolute',
+                        zIndex: '1',
+                        bottom: '100%', left: '12.5%',
+                        objectFit: 'cover',
+                        filter: 'contrast(0.9)',
+                    }}
+                    preview={false} />
+            </div>
+            <div className='center-stage-input-blocks'>
+                <div className='input-block one'>
+                    <p className='input-block-title'>Title</p>
+                </div>
+                <div className='input-block two'>
+                    <p className='input-block-title'>Description</p>
+                </div>
+                <div className='input-block three'>
+                    <p className='input-block-title'>Tags</p>
+                </div>
             </div>
         </div>
     )
